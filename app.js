3fd15c8a5c737bb5ef8f3eda7d299d52299d46d5
AnserrrR <shiparev2002@gmail.com>
2021-06-12 16:30:26 +0300

    MySql credentials

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const path = require('path')
 
 const port = process.env.PORT || 3000
 
-const sequelize = new Sequelize('application_base', 'user', '12345', {
-    host: 'localhost',
+const sequelize = new Sequelize('xgzi1skwph8r0m1r', 'fcro6mxjlpctfyiq	', 'm5swpf29c63798d4', {
+    host: 'd3y0lbg7abxmbuoi.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
     dialect: 'mysql'
 });
 
